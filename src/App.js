03376138a74516151a789a77925511ec862e8da5
Bryan Grill <grillb@gmail.com>
2018-11-17 07:39:17 -0500

    add auth flow

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import StravaAuth from './StravaAuth';
 import './App.css';
 
+const SESSION = '__STRAVA__SESSION__';
 
-// TODO: convert to function, read and parse url query from strava
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <StravaAuth />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const readLocal = () => {
+  const local = localStorage.getItem(SESSION);
+  return JSON.parse(local);
+};
+
+const refresh = async session => {
+  const token = await axios.post(
+    'https://www.strava.com/oauth/token',
+    {},
+    {
+      params: {
+        refresh_token: session.refresh_token,
+        client_id: process.env.REACT_APP_ID,
+        client_secret: process.env.REACT_APP_SECRET,
+        grant_type: 'refresh_token',
+      },
+    },
+  );
+  return token.data;
+};
+
+const App = () => {
+  const [session, setSession] = useState(null);
+
+  useEffect(async () => {
+    console.log('USE EFFECT!')
+    const local = readLocal();
+    if (local) {
+      console.log(local);
+      const session = await refresh(local)
+      setSession(session);
+    }
+  }, []);
+
+  const onExchange = session => {
+    localStorage.setItem(SESSION, JSON.stringify(session));
+    setSession(session);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {session ? <div>Streak</div> : <StravaAuth onExchange={onExchange} />}
+      </header>
+    </div>
+  );
+};
 
 export default App;
--- src/StravaAuth.js
+++ src/StravaAuth.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import qs from 'qs';
+import axios from 'axios';
 import stravaBtn from './strava.svg';
-const href = `https://www.strava.com/oauth/authorize?client_id=12564&response_type=code&redirect_uri=http://localhost:3000&approval_prompt=auto&scope=activity:read_all`;
+const href = `https://www.strava.com/oauth/authorize?client_id=${
+  process.env.REACT_APP_ID
+}&response_type=code&redirect_uri=http://localhost:3000&approval_prompt=auto&scope=activity:read_all`;
 
-const StravaConnect = () => {
+const exchange = async code => {
+  const token = await axios.post(
+    'https://www.strava.com/oauth/token',
+    {},
+    {
+      params: {
+        code,
+        client_id: process.env.REACT_APP_ID,
+        client_secret: process.env.REACT_APP_SECRET,
+        grant_type: 'authorization_code',
+      },
+    },
+  );
+  return token.data;
+};
+
+const authFlow = async onExchange => {
+  const search = qs.parse(window.location.search);
+  if (search.code) {
+    console.log('request access token');
+    console.log(search);
+    window.history.replaceState({}, document.title, '/');
+    const token = await exchange(search.code);
+    console.log(token);
+    onExchange(token);
+  }
+};
+
+const StravaConnect = ({ onExchange }) => {
+  useEffect(() => {
+    authFlow(onExchange);
+  });
+
   return (
     <a href={href}>
       <img alt="strava oauth" src={stravaBtn} />
