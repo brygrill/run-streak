939ecc3d3bd17ea0212f74ce55be1cc09955f4c3
Bryan Grill <grillb@gmail.com>
2018-11-21 16:53:47 -0500

    fetch activities

---
+++ src/Count.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Count = ({ count }) => {
+  return <div style={{ fontSize: '18rem' }}>{count}</div>;
+};
+
+export default Count;
--- src/Streak.js
+++ src/Streak.js
@@ -1,8 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Streak = ({token}) => {
-  console.log(token)
-  return <div>Streak</div>
-}
+import Count from './Count';
+
+const fetchActivities = async token => {
+  const activities = await axios.get(
+    'https://www.strava.com/api/v3/athlete/activities',
+    {
+      headers: { Authorization: `Bearer ${token}` },
+      params: {
+        per_page: 200,
+      },
+    },
+  );
+  return activities.data;
+};
+
+const Streak = ({ token }) => {
+  const [count, setCount] = useState({
+    count: 0,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(async () => {
+    try {
+      const count = await fetchActivities(token.access_token);
+      console.log(count);
+      setCount({ count: 5, loading: false, error: null });
+    } catch (error) {
+      setCount({ count: 0, loading: false, error });
+    }
+  }, []);
+
+  if (count.loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (count.error) {
+    return <div>Error...</div>;
+  }
+
+  return <Count count={count.count} />;
+};
 
 export default Streak;
